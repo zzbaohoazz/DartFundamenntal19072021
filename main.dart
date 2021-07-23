4c8b7ad58c972ef2fd58d46e3249b5bdaf3f20bd
bao hoa <randy.baohoa2017@gmail.com>
2021-07-23 15:19:21 +0700

    update

---
+++ main.dart
@@ -0,0 +1,21 @@
+void main (){
+
+  // kết nói chuổi lại với nhau
+  String a = " a ";
+  String b = " b ";
+  String c = " c ";
+
+  StringBuffer buffer = StringBuffer();
+  buffer.write(a);
+  buffer.write("Trần ");
+  buffer.write(b);
+  buffer.write("Bảo");
+  buffer.write(c);
+  buffer.write("Hoà");
+
+  print(buffer.toString());
+
+
+
+
+}
